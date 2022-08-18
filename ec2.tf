f3ebbb4f3948c2bf7678fa45969f6199d82cfc20
prabusampath007 <79634402+prabusampath007@users.noreply.github.com>
2022-08-18 23:48:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,9 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
+  tags = {
+    Name = "prabu-instance"
+  }
 }
 
 resource "aws_instance" "myec2_1" {
