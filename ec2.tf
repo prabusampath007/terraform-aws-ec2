2f333a4931ea08d1f5664ae4a6a9e8c6229240e0
prabusampath007 <79634402+prabusampath007@users.noreply.github.com>
2022-08-18 23:26:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "myec2" {
   ami = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "myec2_1" {
+  ami = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+}
