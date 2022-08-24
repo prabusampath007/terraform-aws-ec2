0572dbd264c24fcc87ebfd3b386f01571c324202
prabu <ps@presidio.com>
2022-08-24 18:59:01 +0530

    Added readme

--- webapp/main.js
+++ webapp/main.js
@@ -1,7 +1,7 @@
 const http = require("http");
 var mysql = require("mysql2");
 const hostname = "0.0.0.0";
-const webAppPort = process.env.WEB_APP_PORT;
+const webAppPort = 80;
 
 const server = http.createServer((req, res) => {
   res.end(JSON.stringify("hello"))
