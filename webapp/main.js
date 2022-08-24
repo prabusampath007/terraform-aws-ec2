b231a3e4356a818822a503ac355be8a0c82876fe
prabu <ps@presidio.com>
2022-08-24 18:11:01 +0530

    Updated webapp

--- webapp/main.js
+++ webapp/main.js
@@ -2,30 +2,9 @@ const http = require("http");
 var mysql = require("mysql2");
 const hostname = "0.0.0.0";
 const webAppPort = process.env.WEB_APP_PORT;
-const mysqlPassword = process.env.MYSQL_PASSWORD
-const mysqlHostname = process.env.MYSQL_HOSTNAME
 
 const server = http.createServer((req, res) => {
-  let dbResult;
-  var con = mysql.createConnection({
-    host: mysqlHostname,
-    user: "root",
-    password: mysqlPassword,
-    port: 3306,
-  });
-  con.connect(function (err) {
-    if (err) console.log(err);
-    console.log("Connected!");
-    var sql = "select * from app.user";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      dbResult = result;
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      console.log(dbResult);
-      res.end(JSON.stringify(dbResult));
-    });
-  });
+  res.end(JSON.stringify("hello"))
 });
 
 server.listen(webAppPort, hostname, () => {
